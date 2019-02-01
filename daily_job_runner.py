c34f5e9ea2bc4b1d3dc89586586b32973615b951
Nick Guan <guanbeiqi@gmail.com>
2019-01-31 23:37:17 -0800

    Added daily job runner.

---
+++ daily_job_runner.py
@@ -0,0 +1,23 @@
+import schedule
+import time
+import scrawl
+import os
+
+def job():
+  global job_count
+  job_start_time = time.time()
+  print('Running daily job %d' % job_count);
+  scrawl.start_scrawling(firstTime=True) if job_count == 0 else scrawl.start_scrawling()
+  job_end_time = time.time();
+  print('Job %d finishes in %d' % (job_count, job_end_time-job_start_time))
+  job_count += 1
+
+if __name__ == "__main__":
+  print('Starting...');
+  job_count = 0
+  # schedule.every().day.do(job);
+
+  job()
+  # while(True):
+  #   schedule.run_pending();
+  #   time.sleep(600);
